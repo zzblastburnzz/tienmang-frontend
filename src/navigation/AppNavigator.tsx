6308d725e51ef4737077e53a42222610216ad838
Ant <thanhpc.luxuryhome@gmail.com>
2025-04-25 20:36:18 +0700

    up

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import FeedScreen from '.src/screens/FeedScreen';
-import ChatListScreen from '.src/screens/ChatListScreen';
-import FactionScreen from '.src/screens/FactionScreen';
-import MyProfileScreen from '.src/screens/MyProfileScreen';
+import ChatListScreen from './src/screens/ChatListScreen';
+import FeedScreen from './src/screens/FeedScreen';
+import FactionScreen from './src/screens/FactionScreen';
+import MyProfileScreen from './src/screens/MyProfileScreen';
+import CharacterProfileScreen from './src/screens/CharacterProfileScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
-export default function AppNavigator() {
+function MainTabs() {
   return (
     <Tab.Navigator initialRouteName="Feed">
       <Tab.Screen name="Chat" component={ChatListScreen} options={{ title: 'Trò chuyện' }} />
@@ -15,5 +18,14 @@ export default function AppNavigator() {
       <Tab.Screen name="Faction" component={FactionScreen} options={{ title: 'Tông phái' }} />
       <Tab.Screen name="Profile" component={MyProfileScreen} options={{ title: 'Cá nhân' }} />
     </Tab.Navigator>
+  );
+}
+
+export default function AppNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={MainTabs} options={{ headerShown: false }} />
+      <Stack.Screen name="CharacterProfile" component={CharacterProfileScreen} options={{ title: 'Hồ sơ nhân vật' }} />
+    </Stack.Navigator>
   );
 }
